5fbde7bfd6cdae91b0ed57bcdc6c0cca4f6b2fea
nizam <keramov-nizam@yandex.ru>
2023-05-29 03:22:17 +0500

    Updated install_nifi.sh script

--- install_nifi.sh
+++ install_nifi.sh
@@ -1,3 +1,14 @@
+#!/usr/bin/env bash
+
+
+echo_with_red_color() {
+  echo -e '\e[1;31m'$1'\e[m';
+}
+
+echo_with_cyan_color() {
+  echo -e '\e[1;36m'$1'\e[m';
+}
+
 download_cmd="wget"
 download_threads_count=5
 
@@ -6,155 +17,182 @@ if [[ $# -eq 1 ]]; then
   VERSION_REGEX="^(0|[1-9][0-9]*)\\.(0|[1-9][0-9]*)\\.(0|[1-9][0-9]*)$"
   echo "passed version - $NIFI_VERSION"
   if [[ "$NIFI_VERSION" =~ $VERSION_REGEX ]]; then
-
+    # Selecting download command
     if ! command -v aria2c &> /dev/null
     then
       echo "aria2c is not installed, wget will be used for downloads"
-
     else
       echo "aria2c is installed and will be used for downloads"
       download_cmd="aria2c -x${download_threads_count} --summary-interval=0"
     fi
-    
-    # download and extract NiFi, NiFi Registry, NiFi Toolkit
+    # Creating temp dir
+    dir_path="$(uuidgen)"
     cd ~
-    dirname=$(uuidgen)
-    mkdir ${dirname}
-    cd ${dirname}
-    echo "current dirname is $(pwd)"
-    printf "\n" >> ~/.profile
-    echo -n "========================= Downloading and extracting Apache NiFi =========================="
+    mkdir ${dir_path}
+    cd ${dir_path}
+    echo "current dir is $(pwd)"
+    # Downloading and extracting NiFi, NiFi Registry, NiFi Toolkit
+    echo_with_cyan_color "❯❯❯  Downloading and extracting Apache NiFi"
     eval "${download_cmd} https://dlcdn.apache.org/nifi/${NIFI_VERSION}/nifi-${NIFI_VERSION}-bin.zip"
     unzip -q nifi-${NIFI_VERSION}-bin.zip -d /opt/
-    echo "export NIFI_HOME=/opt/nifi-${NIFI_VERSION}" >> ~/.profile
-
-    echo -n "===================== Downloading and extracting Apache NiFi Registry ====================="
+    echo_with_cyan_color "❯❯❯  Downloading and extracting Apache NiFi Registry"
     eval "${download_cmd} https://dlcdn.apache.org/nifi/$NIFI_VERSION/nifi-registry-${NIFI_VERSION}-bin.zip"
     unzip -q nifi-registry-${NIFI_VERSION}-bin.zip -d /opt/
-    echo "export NIFI_REGISTRY_HOME=/opt/nifi-registry-${NIFI_VERSION}" >> ~/.profile
-
-    echo -n "====================== Downloading and extracting Apache NiFi Toolkit ====================="
+    echo_with_cyan_color "❯❯❯  Downloading and extracting Apache NiFi Toolkit"
     eval "${download_cmd} https://dlcdn.apache.org/nifi/$NIFI_VERSION/nifi-toolkit-${NIFI_VERSION}-bin.zip"
     unzip -q nifi-toolkit-${NIFI_VERSION}-bin.zip -d /opt/
+    # Removing temp dir
+    cd ~ && rm -rf ${dir_path}
+    # Updating envinroment variables
+    echo_with_cyan_color "❯❯❯  Adding envinroment variables"
+    source ~/.profile    
+    if ! [[ -z "${NIFI_HOME}" ]]; then
+      sed -i '/^export NIFI_HOME/d' ~/.profile;
+    fi
+    if ! [[ -z "${NIFI_REGISTRY_HOME}" ]]; then
+      sed -i '/^export NIFI_REGISTRY_HOME/d' ~/.profile;
+    fi
+    if ! [[ -z "${NIFI_TOOLKIT_HOME}" ]]; then
+      sed -i '/^export NIFI_TOOLKIT_HOME/d' ~/.profile;
+    fi
+    printf "\n" >> ~/.profile
+    echo "export NIFI_HOME=/opt/nifi-${NIFI_VERSION}" >> ~/.profile
+    echo "export NIFI_REGISTRY_HOME=/opt/nifi-registry-${NIFI_VERSION}" >> ~/.profile
     echo "export NIFI_TOOLKIT_HOME=/opt/nifi-toolkit-${NIFI_VERSION}" >> ~/.profile
     printf "\n" >> ~/.profile
-
     source ~/.profile
-
-    # If NiFi and NiFi Registry are not installed as a service
+    
+    # Updating aliases for commands (for case when NiFi and NiFi Registry are not installed as a service)
+    echo_with_cyan_color "❯❯❯  Adding aliases for commands"
+    source ~/.bashrc
+    if [ "$(type -t NIFI_START)" = 'alias' ]; then
+      sed -i '/^alias NIFI_START/d' ~/.bashrc;
+    fi
+    if [ "$(type -t NIFI_STOP)" = 'alias' ]; then
+      sed -i '/^alias NIFI_STOP/d' ~/.bashrc;
+    fi
+    if [ "$(type -t NIFI_RESTART)" = 'alias' ]; then
+      sed -i '/^alias NIFI_RESTART/d' ~/.bashrc;
+    fi
+    if [ "$(type -t NIFI_STATUS)" = 'alias' ]; then
+      sed -i '/^alias NIFI_STATUS/d' ~/.bashrc;
+    fi
     printf "\n" >> ~/.bashrc
     echo "alias NIFI_START=\"$NIFI_HOME/bin/nifi.sh start\"" >> ~/.bashrc
     echo "alias NIFI_STOP=\"$NIFI_HOME/bin/nifi.sh stop\"" >> ~/.bashrc
     echo "alias NIFI_RESTART=\"$NIFI_HOME/bin/nifi.sh restart\"" >> ~/.bashrc
     echo "alias NIFI_STATUS=\"$NIFI_HOME/bin/nifi.sh status\"" >> ~/.bashrc
+    if [ "$(type -t NIFI_REGISTRY_START)" = 'alias' ]; then
+      sed -i '/^alias NIFI_REGISTRY_START/d' ~/.bashrc;
+    fi
+    if [ "$(type -t NIFI_REGISTRY_STOP)" = 'alias' ]; then
+      sed -i '/^alias NIFI_REGISTRY_STOP/d' ~/.bashrc;
+    fi
+    if [ "$(type -t NIFI_REGISTRY_RESTART)" = 'alias' ]; then
+      sed -i '/^alias NIFI_REGISTRY_RESTART/d' ~/.bashrc;
+    fi
+    if [ "$(type -t NIFI_REGISTRY_STATUS)" = 'alias' ]; then
+      sed -i '/^alias NIFI_REGISTRY_STATUS/d' ~/.bashrc;
+    fi
     printf "\n" >> ~/.bashrc
     echo "alias NIFI_REGISTRY_START=\"$NIFI_REGISTRY_HOME/bin/nifi-registry.sh start\"" >> ~/.bashrc
     echo "alias NIFI_REGISTRY_STOP=\"$NIFI_REGISTRY_HOME/bin/nifi-registry.sh stop\"" >> ~/.bashrc
     echo "alias NIFI_REGISTRY_RESTART=\"$NIFI_REGISTRY_HOME/bin/nifi-registry.sh restart\"" >> ~/.bashrc
     echo "alias NIFI_REGISTRY_STATUS=\"$NIFI_REGISTRY_HOME/bin/nifi-registry.sh status\"" >> ~/.bashrc
     printf "\n" >> ~/.bashrc
+    source ~/.bashrc
+    #eval "$(cat ~/.bashrc | tail -n +10)"
 
-
-    # If NiFi and NiFi Registry are installed as a service
-    #sudo $NIFI_HOME/bin/nifi.sh install nifi_service
-    #echo "alias NIFI_START=\"sudo service nifi_service start\"" >> ~/.bashrc
-    #echo "alias NIFI_STOP=\"sudo service nifi_service stop\"" >> ~/.bashrc
-    #echo "alias NIFI_RESTART=\"sudo service nifi_service restart\"" >> ~/.bashrc
-    #echo "alias NIFI_STATUS=\"sudo service nifi_service status\"" >> ~/.bashrc
-    #sudo $NIFI_REGISTRY_HOME/bin/nifi-registry.sh install nifi_registry_service
-    #echo "alias NIFI_REGISTRY_START=\"sudo service nifi_registry_service start\"" >> ~/.bashrc
-    #echo "alias NIFI_REGISTRY_STOP=\"sudo service nifi_registry_service stop\"" >> ~/.bashrc
-    #echo "alias NIFI_REGISTRY_RESTART=\"sudo service nifi_registry_service restart\"" >> ~/.bashrc
-    #echo "alias NIFI_REGISTRY_STATUS=\"sudo service nifi_registry_service status\"" >> ~/.bashrc
-
-
-    #source ~/.bashrc
-    # eval hack that allows to skip the few first lines and evaluates the rest of the ~/.bashrc so the rest is evaluated and modifies the current execution
-    # be aware it is a magic number and might not work across Ubuntu versions
-    eval "$(cat ~/.bashrc | tail -n +10)"
-
-    cd ~
-    rm -rf ${dirname}
-
-    # Change default ui banner text
-    nifi_banner_text=LOCALHOST
-    sed -i "s/^[#]*\s*nifi.ui.banner.text=.*/nifi.ui.banner.text=${nifi_banner_text}/" "$NIFI_HOME/conf/nifi.properties"
-
-    filename="$NIFI_HOME/conf/bootstrap.conf"
+    # Updating NiFi bootstrap.conf settings
+    nifi_bootstrap_filename="$NIFI_HOME/conf/bootstrap.conf"
+    echo_with_cyan_color "❯❯❯  Updating NiFi bootstrap.conf (${nifi_bootstrap_filename})"
     # Change default JVM memory settings
     nifi_min_memory="2G"
     nifi_max_memory="4G"
-    sed -i "s/^[#]*\s*java.arg.2=.*/java.arg.2=-Xms${nifi_min_memory}/" $filename
-    sed -i "s/^[#]*\s*java.arg.3=.*/java.arg.3=-Xmx${nifi_max_memory}/" $filename
-
-    # Change default JVM timzezone (parameter may be missing)
+    sed -i "s/^[#]*\s*java.arg.2=.*/java.arg.2=-Xms${nifi_min_memory}/" $nifi_bootstrap_filename
+    sed -i "s/^[#]*\s*java.arg.3=.*/java.arg.3=-Xmx${nifi_max_memory}/" $nifi_bootstrap_filename
+    echo "Updated JVM settings '-Xms' to '${nifi_min_memory}' and '-Xmx' to '${nifi_max_memory}'"
+    # Change default JVM timezone (parameter may be absent)
     key_name="java.arg.8"
     key_value="-Duser.timezone=Etc/UTC"
-    if ! grep -R "^[#]*\s*${key_name}=.*" $filename > /dev/null; then
-      echo "Appending '${key_name}' to '${filename}' because not found"
-      printf "\n" >> $filename
-      echo "$key_name=$key_value" >> $filename
+    if ! grep -R "^[#]*\s*${key_name}=.*" $nifi_bootstrap_filename > /dev/null; then
+      echo "Added setting '${key_name}' with value '${key_value}'"
+      printf "\n" >> $nifi_bootstrap_filename
+      echo "$key_name=$key_value" >> $nifi_bootstrap_filename
     else
-      echo "SETTING because '${key_name}' found already"
-      sed -i "s/^[#]*\s*${key_name}=.*/$key_name=$key_value/" $filename
+      echo "Updated setting '${key_name}' to value '${key_value}'"
+      sed -i "s/^[#]*\s*${key_name}=.*/$key_name=$key_value/" $nifi_bootstrap_filename
     fi
-
-    # Change default JVM encodings (parameter may be missing)
+    # Change default JVM encodings (parameter may be absent)
     key_name="java.arg.57"
     key_value="-Dfile.encoding=UTF8"
-    if ! grep -R "^[#]*\s*${key_name}=.*" $filename > /dev/null; then
-      echo "Appending '${key_name}' to '${filename}' because not found"
-      printf "\n" >> $filename
-      echo "$key_name=$key_value" >> $filename
+    if ! grep -R "^[#]*\s*${key_name}=.*" $nifi_bootstrap_filename > /dev/null; then
+      echo "Added setting '${key_name}' with value '${key_value}'"
+      printf "\n" >> $nifi_bootstrap_filename
+      echo "$key_name=$key_value" >> $nifi_bootstrap_filename
     else
-      echo "SETTING because '${key_name}' found already"
-      sed -i "s/^[#]*\s*${key_name}=.*/$key_name=$key_value/" $filename
+      echo "Updated setting '${key_name}' to value '${key_value}'"
+      sed -i "s/^[#]*\s*${key_name}=.*/$key_name=$key_value/" $nifi_bootstrap_filename
     fi
-
-    # Fixed encodings for cyrillic symbols (parameter may be missing)
+    # Fixed encodings for cyrillic symbols (parameter may be absent)
     key_name="java.arg.58"
     key_value="-Dcalcite.default.charset=utf-8"
-    if ! grep -R "^[#]*\s*${key_name}=.*" $filename > /dev/null; then
-      echo "Appending '${key_name}' to '${filename}' because not found"
-      printf "\n" >> $filename
-      echo "$key_name=$key_value" >> $filename
+    if ! grep -R "^[#]*\s*${key_name}=.*" $nifi_bootstrap_filename > /dev/null; then
+      echo "Added setting '${key_name}' with value '${nifi_bootstrap_filename}'"
+      printf "\n" >> $key_value
+      echo "$key_name=$key_value" >> $nifi_bootstrap_filename
     else
-      echo "SETTING because '${key_name}' found already"
-      sed -i "s/^[#]*\s*${key_name}=.*/$key_name=$key_value/" $filename
+      echo "Updated setting '${key_name}' to value '${key_value}'"
+      sed -i "s/^[#]*\s*${key_name}=.*/$key_name=$key_value/" $nifi_bootstrap_filename
     fi
 
+
+
+    # Updading NiFi nifi.properties settings
+    nifi_properties_filename="$NIFI_HOME/conf/nifi.properties"
+    echo_with_cyan_color "❯❯❯  Updating NiFi nifi.properties (${nifi_properties_filename})"
+    # Change default UI banner text
+    nifi_banner_text="LOCALHOST"
+    sed -i "s/^[#]*\s*nifi.ui.banner.text=.*/nifi.ui.banner.text=${nifi_banner_text}/" $nifi_properties_filename
+    echo "Updated setting 'nifi.ui.banner.text' to value '${nifi_banner_text}'"
+    
     nifi_sensitive_key=$(openssl rand -hex 20)
-    echo "NiFi sensitive key = ${nifi_sensitive_key}"
+    # Change sensitive key
     eval "$NIFI_HOME/bin/nifi.sh set-sensitive-properties-key ${nifi_sensitive_key} &> /dev/null"
-
-    nifi_login=nifi
-    nifi_password=nifi_password
-
-    echo "NiFi login = ${nifi_login}"
-    echo "NiFi passsword = ${nifi_password}"
+    echo "Updated setting 'nifi.sensitive.props.key' to value '${nifi_sensitive_key}'"
+    nifi_login="nifi"
+    nifi_password="nifi_password"
+    echo "NiFi Login = ${nifi_login}"
+    echo "NiFi Passsword = ${nifi_password}"
     eval "$NIFI_HOME/bin/nifi.sh set-single-user-credentials ${nifi_login} ${nifi_password} &> /dev/null"
-
+    # Start NiFi and wait
     eval "$NIFI_HOME/bin/nifi.sh start &> /dev/null"
     echo -n "Waiting for NiFi start ..."
     k=1
-    kmax=60
-    nifi_link=""
-    while [[ $k -le $kmax ]] && [[ "$nifi_link" = "" ]]
+    kmax=150
+    nifi_web_interface_link=""
+    while [[ $k -le $kmax ]] && [[ "$nifi_web_interface_link" = "" ]]
     do
-            nifi_link=$(grep "org.apache.nifi.web.server.JettyServer https" $NIFI_HOME/logs/nifi-app*.log | tail -1 | grep -Eo '(http|https)://[^/"]+')
+            nifi_web_interface_link=$(grep "org.apache.nifi.web.server.JettyServer https" $NIFI_HOME/logs/nifi-app*.log | tail -1 | grep -Eo '(http|https)://[^/"]+')
             echo -n '.'
             ((k=k+1))
             sleep 1
     done
     printf "\n"
-    echo "NiFi started at ${nifi_link}"
+    if [ "$k" -le $kmax ];
+      then
+        echo_with_cyan_color "❯❯❯  NiFi started at ${nifi_web_interface_link}/nifi/"
+      else
+        echo_with_red_color "ERROR, NiFi not started"
+    fi
+    
     #echo "To get the username and password use the command: grep Generated $NIFI_HOME/logs/nifi-app*log"
   else
-		echo "passed version did not match version regex"
+		echo_with_red_color "ERROR, passed version did not match version regex"
 	fi
 else
-	echo "error, version is not passed or passed more than one argument"
-fi	
+	echo_with_red_color "ERROR, version is not passed or passed more than one argument"
+fi
 
 
 
--- system_config_nifi.sh
+++ system_config_nifi.sh
@@ -4,6 +4,18 @@ if [ "$(id -u)" != 0 ]; then
   exit 1
 fi
 
+# If NiFi and NiFi Registry are installed as a service
+#sudo $NIFI_HOME/bin/nifi.sh install nifi_service
+#echo "alias NIFI_START=\"sudo service nifi_service start\"" >> ~/.bashrc
+#echo "alias NIFI_STOP=\"sudo service nifi_service stop\"" >> ~/.bashrc
+#echo "alias NIFI_RESTART=\"sudo service nifi_service restart\"" >> ~/.bashrc
+#echo "alias NIFI_STATUS=\"sudo service nifi_service status\"" >> ~/.bashrc
+#sudo $NIFI_REGISTRY_HOME/bin/nifi-registry.sh install nifi_registry_service
+#echo "alias NIFI_REGISTRY_START=\"sudo service nifi_registry_service start\"" >> ~/.bashrc
+#echo "alias NIFI_REGISTRY_STOP=\"sudo service nifi_registry_service stop\"" >> ~/.bashrc
+#echo "alias NIFI_REGISTRY_RESTART=\"sudo service nifi_registry_service restart\"" >> ~/.bashrc
+#echo "alias NIFI_REGISTRY_STATUS=\"sudo service nifi_registry_service status\"" >> ~/.bashrc
+
 printf "\n" >> /etc/security/limits.conf
 printf "* hard nofile 50000\n" >> /etc/security/limits.conf
 printf "* soft nofile 50000\n" >> /etc/security/limits.conf
